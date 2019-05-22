888a9806acee55973c45eae943dca5831c9dff8d
Orr Benyamini <orr.bob@hotmail.com>
2019-05-22 18:44:27 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 26, 2019
+ *      Author: orrbo
+ */
+#include <stdio.h>
+#include "SPBufferset.h"
+#include "main_aux.h"
+
+#define MAX_HEAPS = 32
+
+int main(void) {
+
+}
