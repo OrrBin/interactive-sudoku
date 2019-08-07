8e8dbead973de67b48d02d9e8a8f913c0553ce1c
Yarden <Yarden@DESKTOP-SH3671O>
2019-08-07 15:03:42 +0300

    change initGame to get no parameters. it will ask the user from the desired parameters(block height, block width).

--- main.c
+++ main.c
@@ -14,35 +14,22 @@
 #include "parser.h"
 #include "stack.h"
 
-int main() {
-	int i=5;
-	int *x;
+int main(int argc, char* argv[]) {
 
-	struct Stack *stack;
-	stack = createStack();
-	printf("%d\n" ,isEmpty(stack));
-	push(stack, &i);
-	printf("%d" ,isEmpty(stack));
-	x=(int*) pop(stack);
-	printf("%d", *x);
-	return 0;
-
-/*	Board* board;
+	Board* board;
 	int seed = atoi(argv[1]);
 	srand(seed);
 
-	SP_BUFF_SET()
-	;
-
+	SP_BUFF_SET();
 	setbuf(stdout, NULL);
 
 	if (argc < 1) {
 		exit(-1);
 	}
 
-	board = initGame(10, 2, 5);
+	board = initGame();
 
 	while (1) {
 		playTurn(&board);
-	}*/
+	}
 }
